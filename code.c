c57d817cddad6172b2e407661418bfa3e5e1916d
edupazzz <du.passos51@gmail.com>
2022-06-03 23:54:22 -0300

    First try...

---
+++ code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	// Just do some usefules code...
+
+	if(argc < 2)
+	{ printf("\nType a message...\n"); 
+  	exit(0);
+	}
+  
+  printf("\n\t\t\t %s", argv[1]);
+	puts("\n\t____________________/_");
+  puts("\t|       // o O \\\\  /  |\n\t|           |+        |\n\t|       \\\\ --- //     |\n\t|   ----        ----  |\n\t|  |    |\n\t|\n\t|\n\t|\n\t|");
+
+	puts("\t   M@DM4N1NTH3W1ND0WN\n");
+}
